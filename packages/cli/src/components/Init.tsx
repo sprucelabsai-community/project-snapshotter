93de0f4bcf462f5340bf6e423acc8f2c4336d9e7
Taylor Romero <me@taylorrome.ro>
2026-01-13 12:36:41 -0700

    patch: export test

--- packages/cli/src/components/Init.tsx
+++ packages/cli/src/components/Init.tsx
@@ -1,6 +1,6 @@
 import { spawnSync } from 'node:child_process'
 import { existsSync, readFileSync } from 'node:fs'
-import { RegressionProofClient } from '@regressionproof/client'
+import RegressionProofClient from '@regressionproof/client'
 import { Box, Text, useApp } from 'ink'
 import BigText from 'ink-big-text'
 import TextInput from 'ink-text-input'
